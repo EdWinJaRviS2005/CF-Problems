3b9e8aeb62e08e2da385b6cd1dd1cc381118336f
Jarvis <hrishikeshpatil5002@gmail.com>
2026-08-06 12:02:21 +0530

    Add To My Critics [A] from Codeforces

---
+++ 1850/A - To My Critics/solution.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define f(i, a, b) for (auto i = a; i <= b; i++)
+#define fr(i, a, b) for (auto i = a; i >= b; i--)
+// #define push_back(i) PB(auto i);
+#define fast std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define endl "
+"
+#define int long long int
+#define all(a) a.begin(),a.end()
+#define time_taken 1.0 * clock() / CLOCKS_PER_SEC
+typedef vector < int > vi;
+ 
+template<typename T>
+void read(T &x) {
+    cin >> x;
+}
+ 
+template<typename T, typename... Args>
+void read(T &x, Args&... args) {
+    cin >> x;
+    read(args...);
+}
+ 
+#define INT(...)   \
+  int __VA_ARGS__; \
+  read(__VA_ARGS__)
+ 
+ 
+///////////////// Actual Code //////////////////////
+ 
+void solve()
+{
+    vi v(3);
+ 
+    f(i, 0, 2) cin >> v[i];
+ 
+    sort(all(v));
+ 
+    if (v[2] + v[1] >= 10) cout << "YES
+";
+    else cout << "NO
+";
+    
+}
+ 
+//////////////// Main function /////////////////////
+ 
+signed main()
+{
+    fast;
+ 
+    INT(tc);
+ 
+    while (tc--)
+ 
+        solve();
+    // cout << time_taken;
+}
+ 
